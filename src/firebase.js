241b9c82e3c94d4fca9a7c093748f2ca4492dd2a
Sebby80 <sebkm80@gmail.com>
2021-08-25 13:49:08 +0530

    Amazon clone setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjzlZTQyms5vwIxyhzkbLiMfgvuCNtUfA",
+  authDomain: "fir-b8b03.firebaseapp.com",
+  projectId: "fir-b8b03",
+  storageBucket: "fir-b8b03.appspot.com",
+  messagingSenderId: "1086067087940",
+  appId: "1:1086067087940:web:dfaeda32504652b791fa11",
+};
