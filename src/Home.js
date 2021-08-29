e0d63d41d62baebaa005824fe773e7878bec016b
Sebby80 <sebkm80@gmail.com>
2021-08-29 15:11:37 +0530

    Giving unique ids to each product and adding zoom feature on hover of products

--- src/Home.js
+++ src/Home.js
@@ -11,16 +11,20 @@ const Home = () => {
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt=""
         />
+        <img
+          className={`${classes["home__image"]}`}
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2020/X-site/Multititle/Aug/1500x600_Hero-Tall_np._CB404803728_.jpg"
+        />
         <div className={`${classes["home__row"]}`}>
           <Product
-            id="12321341"
+            id={Math.random().toString()}
             title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
             price={11.96}
             rating={5}
             image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
           />
           <Product
-            id="49538094"
+            id={Math.random().toString()}
             title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
             price={239.0}
             rating={4}
@@ -29,21 +33,21 @@ const Home = () => {
         </div>
         <div className={`${classes["home__row"]}`}>
           <Product
-            id="4903850"
+            id={Math.random().toString()}
             title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
             price={199.99}
             rating={3}
             image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
           />
           <Product
-            id="23445930"
+            id={Math.random().toString()}
             title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
             price={98.99}
             rating={5}
             image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
           />
           <Product
-            id="3254354345"
+            id={Math.random().toString()}
             title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
             price={598.99}
             rating={4}
@@ -52,7 +56,7 @@ const Home = () => {
         </div>
         <div className={`${classes["home__row"]}`}>
           <Product
-            id="90829332"
+            id={Math.random().toString()}
             title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
             price={1094.98}
             rating={4}
