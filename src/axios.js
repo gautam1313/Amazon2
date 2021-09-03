599669c4251ec64bdf00624f28f9979457648769
Sebby80 <sebkm80@gmail.com>
2021-09-03 17:32:57 +0530

    Add your own baseUrl

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/fir-b8b03/us-central1/api",
+  //Run 'firebase emulators:start' inside functions use the url(api endpoint) provided below
+  baseURL: "",
 });
 
 export default instance;
